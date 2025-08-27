1a63095786c1a57cc9e86b03ec68d26215247239
Xaxis <william.neeley@gmail.com>
2025-08-26 22:49:25 -0800

    checkpoint

--- src/components/AudioSystem.tsx
+++ src/components/AudioSystem.tsx
@@ -224,7 +224,7 @@ const AudioSystem: React.FC = () => {
                 🎤 Your Microphone
               </label>
               <select
-                value={selectedMicrophone}
+                value={selectedMicrophone || ''}
                 onChange={(e) => setSelectedMicrophone(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
               >
@@ -246,7 +246,7 @@ const AudioSystem: React.FC = () => {
                 🔊 Your Speakers
               </label>
               <select
-                value={selectedSpeaker}
+                value={selectedSpeaker || ''}
                 onChange={(e) => setSelectedSpeaker(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
               >
